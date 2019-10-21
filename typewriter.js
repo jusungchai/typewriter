f60dc7d476a22526e606d854fc28a125b540eb35
Jay Chai <chunsaa@gmail.com>
2019-10-21 16:47:00 +0000

    refactored

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 let delay = 0;
 let index = 0;
-function animate(char, interval) {
+for (const char of sentence) {
   setTimeout(() => {
     index++;
     process.stdout.write(char);
     if (index === sentence.length){
       process.stdout.write("\n");
     }
-  }, interval)
-}
-
-for (const char of sentence) {
-  animate(char, delay);
+  }, delay)
   delay += 50;  
 }
